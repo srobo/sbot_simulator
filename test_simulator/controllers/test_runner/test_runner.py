5ddc561b233cd9f8356c93061b740426ee5d9770
WillB97 <will@rudi.org.uk>
2024-05-19 22:07:39 +0100

    Add vision stress test

--- test_simulator/controllers/test_runner/test_runner.py
+++ test_simulator/controllers/test_runner/test_runner.py
@@ -1,8 +1,12 @@
 import os
+import socket
 import sys
+import threading
 from pprint import pprint
 
-from controller import Robot
+from controller import Robot, Camera
+
+FRAME_TIME = 64
 
 robot = Robot()
 
@@ -24,3 +28,29 @@ pprint(dict(os.environ))
 print('')
 print("Path:")
 pprint(sys.path)
+
+camera: Camera = robot.getDevice("camera")
+camera.enable(FRAME_TIME)
+
+def test_recv_setup():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.bind(('localhost', 0))
+    soc.listen(1)
+    return soc
+
+def recv_runner(soc):
+    while True:
+        client = soc.accept()
+        while client[0].recv(4096):
+            pass
+
+srv_soc = test_recv_setup()
+srv_port = srv_soc.getsockname()[1]
+threading.Thread(target=recv_runner, args=(srv_soc,)).start()
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.connect(("localhost", srv_port))
+while robot.step(FRAME_TIME) != -1:
+    image = camera.getImage()
+    soc.sendall(bytes(len(image)))
+    soc.sendall(image)
