a62eb5aa996c8949f7a6917f7ce6a20eb932d49b
WillB97 <will@rudi.org.uk>
2024-06-15 20:43:35 +0100

    Improve helper scripts Keep terminal open on failure

--- scripts/run_simulator.py
+++ scripts/run_simulator.py
@@ -6,6 +6,7 @@ Largely just a shortcut to running the arena world in Webots.
 Only functional in releases.
 """
 import platform
+import traceback
 from pathlib import Path
 from shutil import which
 from subprocess import Popen
@@ -13,33 +14,41 @@ from subprocess import Popen
 if platform.system() == "Windows":
     from subprocess import CREATE_NEW_PROCESS_GROUP, DETACHED_PROCESS
 
+try:
+    if not (Path(__file__).parent / 'simulator/VERSION').exists():
+        print("This script is only functional in releases.")
+        raise RuntimeError
 
-if not (Path(__file__).parent / 'simulator/VERSION').exists():
-    print("This script is only functional in releases.")
-    exit(1)
+    world_file = Path(__file__).parent / "simulator/worlds/arena.wbt"
 
-world_file = Path(__file__).parent / "simulator/worlds/arena.wbt"
+    webots = which("webots")
 
-webots = which("webots")
+    # Find the webots executable, if it is not in the PATH
+    if webots is None:
+        if platform.system() == "Darwin":
+            webots = "/Applications/Webots.app/Contents/MacOS/webots"
+        elif platform.system() == "Windows":
+            webots = "C:\\Program Files\\Webots\\msys64\\bin\\webotsw.exe"
+        elif platform.system() == "Linux":
+            webots = "/usr/bin/webots"
+        else:
+            print("Unsupported platform.")
+            raise RuntimeError
 
-# Find the webots executable, if it is not in the PATH
-if webots is None:
-    if platform.system() == "Darwin":
-        webots = "/Applications/Webots.app/Contents/MacOS/webots"
-    elif platform.system() == "Windows":
-        webots = "C:\\Program Files\\Webots\\msys64\\bin\\webotsw.exe"
-    elif platform.system() == "Linux":
-        webots = "/usr/bin/webots"
+    if not Path(webots).exists():
+        print("Webots executable not found.")
+        raise RuntimeError
+
+    # Run the world file in Webots, detaching the process so it does not close when this script does
+    if platform.system() == "Windows":
+        Popen([webots, world_file], creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP)
     else:
-        print("Unsupported platform.")
-        exit(1)
-
-if not Path(webots).exists():
-    print("Webots executable not found.")
+        Popen([webots, world_file], start_new_session=True)
+except RuntimeError:
+    input("Press enter to continue...")
     exit(1)
-
-# Run the world file in Webots, detaching the process so it does not close when this script does
-if platform.system() == "Windows":
-    Popen([webots, world_file], creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP)
-else:
-    Popen([webots, world_file], start_new_session=True)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    print(traceback.format_exc())
+    input("Press enter to continue...")
+    exit(1)
--- scripts/setup.py
+++ scripts/setup.py
@@ -8,60 +8,74 @@ It will:
 3. Set the python path in runtime.ini to the virtual environment python
 4. Repopulate the zone 0 folder with basic_robot.py if robot.py is missing
 """
+import logging
+import platform
 import shutil
 from pathlib import Path
 from subprocess import run
 from venv import create
 
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
 
-if (Path(__file__).parent / 'simulator/VERSION').exists():
-    # This is running from a release
-    project_root = Path(__file__).parent
-    requirements = project_root / "simulator/requirements.txt"
+try:
+    if (Path(__file__).parent / 'simulator/VERSION').exists():
+        # This is running from a release
+        project_root = Path(__file__).parent
+        requirements = project_root / "simulator/requirements.txt"
+    else:
+        # This is running from the repository
+        project_root = Path(__file__).parents[1]
+        requirements = project_root / "requirements.txt"
+
+    venv_dir = project_root / "venv"
+
+    logger.info(f"Creating virtual environment in {venv_dir.absolute()}")
+    create(venv_dir, with_pip=True)
+
+    logger.info(f"Installing dependencies from {requirements.absolute()}")
+    if platform.system() == "Windows":
+        pip = venv_dir / "Scripts/pip.exe"
+    else:
+        pip = venv_dir / "bin/pip"
+    run([str(pip), "install", "-r", str(requirements)], cwd=venv_dir)
+
+    logger.info("Setting up Webots Python location")
+
+    runtime_ini = project_root / "simulator/controllers/usercode_runner/runtime.ini"
+    runtime_content = []
+    if runtime_ini.exists():
+        prev_runtime_content = runtime_ini.read_text().splitlines()
+    else:
+        prev_runtime_content = []
+
+    # Remove previous python settings from runtime.ini
+    # Everything between [python] and the next section is removed
+    in_python_section = False
+    for line in prev_runtime_content:
+        if line == "[python]":
+            in_python_section = True
+        elif in_python_section and line.startswith("["):
+            in_python_section = False
+        elif not in_python_section:
+            runtime_content.append(line)
+
+    runtime_content.extend([
+        "",
+        "[python]",
+        f"COMMAND = {(venv_dir / 'bin/python').absolute()}",
+    ])
+
+    runtime_ini.write_text('\n'.join(runtime_content))
+
+    # repopulate zone 0 with example code if robot.py is missing
+    zone_0 = project_root / "zone_0"
+    if not (zone_0 / "robot.py").exists():
+        logger.info("Repopulating zone 0 with example code")
+        zone_0.mkdir(exist_ok=True)
+        shutil.copy(project_root / "example_roobts/basic_robot.py", zone_0 / "robot.py")
+except Exception as e:
+    logger.exception("Setup failed due to an error.")
+    input("An error occurred, press enter to close.")
 else:
-    # This is running from the repository
-    project_root = Path(__file__).parents[1]
-    requirements = project_root / "requirements.txt"
-
-venv_dir = project_root / "venv"
-
-print(f"Creating virtual environment in {venv_dir.absolute()}")
-create(venv_dir, with_pip=True)
-
-print(f"Installing dependencies from {requirements.absolute()}")
-run([str(venv_dir / "bin/pip"), "install", "-r", str(requirements)], cwd=venv_dir)
-
-print("Setting up Webots Python location")
-
-runtime_ini = project_root / "simulator/controllers/usercode_runner/runtime.ini"
-runtime_content = []
-if runtime_ini.exists():
-    prev_runtime_content = runtime_ini.read_text().splitlines()
-else:
-    prev_runtime_content = []
-
-# Remove previous python settings from runtime.ini
-# Everything between [python] and the next section is removed
-in_python_section = False
-for line in prev_runtime_content:
-    if line == "[python]":
-        in_python_section = True
-    elif in_python_section and line.startswith("["):
-        in_python_section = False
-    elif not in_python_section:
-        runtime_content.append(line)
-
-runtime_content.extend([
-    "",
-    "[python]",
-    f"COMMAND = {(venv_dir / 'bin/python').absolute()}",
-])
-
-runtime_ini.write_text('\n'.join(runtime_content))
-
-# repopulate zone 0 with example code if robot.py is missing
-zone_0 = project_root / "zone_0"
-if not (zone_0 / "robot.py").exists():
-    print("Repopulating zone 0 with example code")
-    zone_0.mkdir(exist_ok=True)
-    shutil.copy(project_root / "example_roobts/basic_robot.py", zone_0 / "robot.py")
+    input("Setup complete, press enter to close.")
